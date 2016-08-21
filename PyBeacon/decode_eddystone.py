09644acdfba5f965990e088b4592c1fc9c186f72
Wolf Noble <wolf@wolfspyre.com>
2016-08-21 03:44:09 -0500

    splitout eddystone

--- PyBeacon/decode_eddystone.py
+++ PyBeacon/decode_eddystone.py
@@ -16,7 +16,7 @@ import bluetooth._bluetooth as bluez
 import uuid
 from . import __version__
 from PyBeacon.wpl_cfg_parser import wpl_cfg
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('decode_eddystone')
 
 def decode_eddystone(ad_struct):
     """Ad structure decoder for Eddystone
