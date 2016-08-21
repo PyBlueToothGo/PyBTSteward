a25750f7924dc061b60de17a76f6f580cfb4aeb9
Wolf Noble <wolf@wolfspyre.com>
2016-08-21 03:53:51 -0500

    fix reset cmd

--- reset.sh
+++ reset.sh
@@ -6,4 +6,4 @@ if [[ $(git branch |grep -c foo) -lt 1 ]]; then
   git branch foo
 fi
 git checkout foo&& git reset --hard&& git branch -D master
-git pull; git checkout master&&virtualenv --python=python3 ../PyBeacon&& pip install -r requirements.txt && pip install .&& PyBeacon -s
+git fetch; git checkout master&&virtualenv --python=python3 ../PyBeacon&&. bin/activate&& pip install -r requirements.txt && pip install .&& PyBeacon -s
