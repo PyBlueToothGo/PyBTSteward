32a7707c18de821cc46db2dd5f8b7a89213b6a25
Wolf Noble <wolf@wolfspyre.com>
2016-08-21 20:58:04 -0500

    Start to bolt device awareness into config

--- PyBeacon/PyBeacon.py
+++ PyBeacon/PyBeacon.py
@@ -405,7 +405,7 @@ def stopAdvertising():
 def showVersion():
     print(application_name + " " + version)
 
-def main():
+def main(conf):
     if args.version:
         showVersion()
     else:
@@ -429,4 +429,4 @@ if __name__ == "__main__":
     else:
         logger.setLevel(logging.INFO)
     logger.debug('Config: %r', conf)
-    main()
+    main(conf)
