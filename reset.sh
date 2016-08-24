4910ed73ec5f84ae3f5e0db761eb50351241a81b
Wolf Noble <wolf@wolfspyre.com>
2016-08-24 04:46:24 -0500

    kill lingering hci procs with reset.sh. hacky-as-shit

--- reset.sh
+++ reset.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 source bin/activate
 pip uninstall PyBeacon -y;
+if [[ -f local_config.yml ]]; then
+  LOCALCONFIG=true
+  echo 'backing up local_config'
+  cp local_config.yml /tmp/
+fi
+
 git clean -df
 git reset --hard
 if [[ $(git branch |grep -c foo) -lt 1 ]]; then
   git branch foo
+else
+  git branch -D foo && git branch foo
 fi
+
 if [[ `grep -q 'hcidump' /proc/[[:digit:]]*/cmdline; echo $?` > 0 ]]; then
     echo "Hcidump commands still running. killing them."
     sudo killall hcidump
