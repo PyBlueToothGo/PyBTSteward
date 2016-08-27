85658bfdcee714cac32cfde2f4e2ece62b08a513
Wolf Noble <wolf@wolfspyre.com>
2016-08-27 04:20:51 -0500

    slight fixup to stats

--- PyBeacon/PyBeacon.py
+++ PyBeacon/PyBeacon.py
@@ -374,8 +374,8 @@ def main(conf=init()):
                     sendstat_counter('packets.eddystone', pyBState['packets']['eddystone']['count'], conf['Global']['scan_duration'])
                     sendstat_counter('packets.found', pyBState['packets']['found'], conf['Global']['scan_duration'])
                     sendstat_counter('packets.unknown', pyBState['packets']['unknown']['count'], conf['Global']['scan_duration'])
-                    for device in pyBState['packets']['eddystone']['devices']:
-                        logger.info('counts for %s: %s [%s telem, %s uid]', device, device['count'], device['tlm']['count'], device['uid']['count'] )
+                    for k, v in pyBState['packets']['eddystone']['devices']:
+                        logger.info('counts for %s: %s [%s telem, %s uid]', k, v['count'], v['tlm']['count'], v['uid']['count'] )
                 except KeyError:
                     logger.debug('not sending stats as we got a KeyError from the object')
                 except NameError:
